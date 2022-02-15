c84851c0a0efbe1e9b5b8d961c202bd5d8139316
crose1121 <coreycoding1121@outlook.com>
2022-02-15 07:53:51 -0500

    initial commit

---
+++ src/test/java/com/rose/caresoft/CaresoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rose.caresoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaresoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
